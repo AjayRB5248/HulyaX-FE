b91e4d4cb0fe7edbf8f22be7edeaac3d3876e83b
dibyamagar56 <dibya.magar@portpro.io>
2024-02-11 23:38:20 +0545

    Add: New Design for Event Grid

--- src/sections/events-list/view/event-grid.tsx
+++ src/sections/events-list/view/event-grid.tsx
@@ -1,6 +1,9 @@
 import moment from "moment";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
+import VenueIcon from "src/assets/frontend/images/icons/location.png";
+
+import SACAR from "src/assets/frontend/images/artists/Sacar.jpeg";
 
 const EventGridItem: React.FC<{
   imageUrl: StaticImageData;
@@ -10,11 +13,43 @@ const EventGridItem: React.FC<{
   city: string;
   timeZone: string;
   slug: string;
-}> = ({ imageUrl, date, title, venue, city, timeZone, slug }) => {
-
+  artists: any;
+}> = ({ imageUrl, date, title, venue, city, timeZone, slug, artists }) => {
   return (
     <div className="col-sm-6 col-lg-4">
-      <div className="event-grid">
+      <div className="event-item ml-0 mr-0">
+        <div className="event-top-card">
+          <div className="event-date">
+            <h6 className="date-title">{moment(date).format("D MMM")}</h6>
+          </div>
+          <Link href={`/event-detail/${slug}`}>
+            <Image src={imageUrl} alt="Event Poster" width={800} height={1200} />
+
+            <h4 className="event-title">{title}</h4>
+          </Link>
+        </div>
+
+        <div className="event-bottom-card d-flex flex-column mt-3 p-4">
+          <div className="artist d-flex align-items-center">
+            <Image src={SACAR} alt="Artist Profile" className="artist-profile-img" />
+            <h4 className="artist-name ml-3">
+              {artists[0].artistName}
+              <p className="artist-position">Artist/SongWriter</p>
+            </h4>
+            {/* <button className="theme-button">Buy Ticket</button> */}
+          </div>
+
+          <div className="venue d-flex align-items-center">
+            <Image src={VenueIcon} alt="Venue icon" className="venue-icon" />
+
+            <div className="venue-name ml-3">
+              {venue}, {city}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="event-grid">
         <div className="movie-thumb c-thumb">
           <Link href={`/event-detail/${slug}`}>
             <Image src={imageUrl} alt="event" width={800} height={1200} />
@@ -34,7 +69,7 @@ const EventGridItem: React.FC<{
             </span>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/sections/events/view/events-carousel.tsx
+++ src/sections/events/view/events-carousel.tsx
@@ -4,13 +4,17 @@ import Image, { StaticImageData } from "next/image";
 
 // Events Images
 
-import EventImg1 from "src/assets/frontend/images/event/event01.jpg";
-import EventImg2 from "src/assets/frontend/images/event/event02.jpg";
+import EventImg1 from "src/assets/frontend/images/event/pexels-jonathan-borba-3563171.jpg";
+import EventImg2 from "src/assets/frontend/images/event/pexels-markus-spiske-3806767.jpg";
 import EventImg3 from "src/assets/frontend/images/event/event03.jpg";
 import EventImg4 from "src/assets/frontend/images/event/event04.jpg";
+import SportsEvent from "src/assets/frontend/images/event/sfx308846.jpg";
+
 import EventCarouselItem from "../event-carousel-item";
 import { useState } from "react";
 import { useFetchEvents } from "src/api/events";
+import ArtistImg from "src/assets/frontend/images/event/Sacar.jpeg";
+import VenueIcon from "src/assets/frontend/images/event/icon/event-icon02.png";
 
 interface EventData {
   imageUrl: StaticImageData;
@@ -54,6 +58,22 @@ const eventsData: EventData[] = [
     venue: "27 Montague Street, Sydney",
     tags: ["Now Showing", "Exclusive"],
   },
+  {
+    imageUrl: SportsEvent,
+    date: "28",
+    month: "DEC",
+    title: "SACAR Concert 2024",
+    venue: "27 Montague Street, Sydney",
+    tags: ["Now Showing", "Exclusive"],
+  },
+  {
+    imageUrl: EventImg4,
+    date: "28",
+    month: "DEC",
+    title: "SACAR Concert",
+    venue: "27 Montague Street, Sydney",
+    tags: ["Now Showing", "Exclusive"],
+  },
 ];
 
 const tabItems = [
@@ -80,10 +100,105 @@ const EventsCarousel: React.FC = () => {
   const filteredEvents = events.filter((event: any) => !event.tags?.includes(activeTab));
 
   return (
-    <section className="event-section padding-top padding-bottom bg-four">
-      <div className="container">
+    <section className="event-section bg-four section-wrapper">
+      <div className="container-fluid">
+        {/* New Design */}
+        <div className="section-header-2">
+          <div className="left">
+            <h2 className="title">events</h2>
+            <p>Be sure not to miss these Event today.</p>
+          </div>
+          <ul className="tab-menu">
+            {tabItems.map((tabItem) => (
+              <li className={tabItem.title === activeTab ? "active" : ""} onClick={() => setActiveTab(tabItem.title)}>
+                {tabItem.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="row events-row">
+          {/* Put Carousel Here - Only Featured Images */}
+          <div className="col-12 col-md-4 left-col">
+            <div className="event-grid featured">
+              <div className="movie-thumb c-thumb">
+                <div className="overlay"></div>
+                <a href="#0">
+                  <Image src={EventImg2} alt="Event Image" />
+                </a>
+                <div className="event-date">
+                  <h6 className="date-title">28</h6>
+                  <span>Dec</span>
+                </div>
+                <h5 className="event-title">
+                  <a href="#0">SACAR Australia Concert 2024</a>
+                </h5>
+              </div>
+              <div className="event-desc p-3 d-none">
+                <div className="event-artist d-flex align-items-center">
+                  <span className="artist-img">
+                    <Image src={ArtistImg} alt="Artist Image" />
+                  </span>
+                  <div className="d-flex flex-column ml-3">
+                    <span className="artist-name">Sacar</span>
+
+                    <div className="event-venue">
+                      <i className="fa fa-map-marker"></i>
+                      <span className="venue-name ml-2">327 Montague Street</span>
+                    </div>
+
+                    <button className="theme-button">Buy Ticket</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Put Carousel Here -Other Events Images */}
+          <div className="col-12 col-md-8 right-col">
+            <div className="row">
+              {eventsData.map((event: any) => (
+                <div className="col-6 col-md-4">
+                  <div className="event-grid">
+                    <div className="movie-thumb c-thumb">
+                      <div className="overlay"></div>
+                      <a href="#0">
+                        <Image src={event.imageUrl} alt="Event Image" />
+                      </a>
+                      <div className="event-date">
+                        <h6 className="date-title">28</h6>
+                        <span>Dec</span>
+                      </div>
+                      <h5 className="event-title">
+                        <a href="#0">{event.title}</a>
+                      </h5>
+                    </div>
+                    <div className="event-desc p-3 d-none">
+                      <div className="event-artist d-flex align-items-center">
+                        <span className="artist-img">
+                          <Image src={ArtistImg} alt="Artist Image" />
+                        </span>
+                        <div className="d-flex flex-column ml-3">
+                          <span className="artist-name">Sacar</span>
+
+                          <div className="event-venue">
+                            <i className="fa fa-map-marker"></i>
+                            <span className="venue-name ml-2">327 Montague Street</span>
+                          </div>
+
+                          <button className="theme-button">Buy Ticket</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* New Design Ends */}
+
         <div className="tab">
-          <div className="section-header-2">
+          {/* <div className="section-header-2">
             <div className="left">
               <h2 className="title">events</h2>
               <p>Be sure not to miss these Event today.</p>
@@ -95,8 +210,8 @@ const EventsCarousel: React.FC = () => {
                 </li>
               ))}
             </ul>
-          </div>
-          <div className="tab-area mb-30-none">
+          </div> */}
+          {/* <div className="tab-area mb-30-none">
             {activeTab === "Now Showing" && (
               <div className={`tab-item active`}>
                 <EventCarouselItem events={filteredEvents} />
@@ -114,7 +229,7 @@ const EventsCarousel: React.FC = () => {
                 <EventCarouselItem events={filteredEvents} />
               </div>
             )}
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
