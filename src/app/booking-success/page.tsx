8ce818e3764bce0a1c94f9ca47081e8b30a35af4
dibyamagar56 <dibya.magar@portpro.io>
2024-02-28 23:20:08 +0545

    Booking Success Page design

---
+++ src/app/booking-success/page.tsx
@@ -0,0 +1,9 @@
+import { BookingSuccess } from "src/sections/success";
+
+export const metadata = {
+  title: "Ticket Booking Success.",
+};
+
+export default function BookingSuccessPage() {
+  return <BookingSuccess />;
+}
