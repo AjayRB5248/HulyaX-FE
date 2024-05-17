a573c06256e2a8f4b7d75cc96ffb1425ba15ed29
dibyamagar56 <dibya.magar@portpro.io>
2024-05-18 02:36:13 +0545

    Fix: Alignment of user avatar and header bar

--- src/layouts/main/header.tsx
+++ src/layouts/main/header.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import Logo from "src/assets/frontend/images/hulya-events-logo.png";
+import UserAccountPopover from "../_common/user-account-popover";
+import { useAuth } from "src/auth/context/users/auth-context";
 
 export default function Header() {
+  const { user } = useAuth();
+
   const [isScrolled, setIsScrolled] = useState(false);
   const [isToggleMenuActive, setIsToggleMenuActive] = useState(false);
 
@@ -41,10 +45,17 @@ export default function Header() {
               </Link>
             </div>
             <Navbar isToggleMenuActive={isToggleMenuActive} />
-            <div className={`header-bar d-lg-none ${isToggleMenuActive ? "active" : ""}`} onClick={handleToggleMenu}>
-              <span></span>
-              <span></span>
-              <span></span>
+
+            <div className="d-flex align-items-center">
+              {user && <UserAccountPopover />}
+              <div
+                className={`header-bar ml-4 d-lg-none ${isToggleMenuActive ? "active" : ""}`}
+                onClick={handleToggleMenu}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
             </div>
           </div>
         </div>
