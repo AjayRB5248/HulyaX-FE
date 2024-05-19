69afa484da3e7cb41f11d4abd96ea6beaa05ed2a
AjayRB5248 <ajayranabhatofficial@gmail.com>
2024-05-19 18:03:28 +0545

    fix

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -20,11 +20,7 @@ const withAuth = (WrappedComponent: React.ComponentType<any>) => {
         if (allowedUserTypes.includes(loggedInUserType)) {
           setVerified(true);
         } else {
-<<<<<<< Updated upstream
-          router.push('/home');
-=======
           router.push('/dashboard');
->>>>>>> Stashed changes
         }
       }
     };
