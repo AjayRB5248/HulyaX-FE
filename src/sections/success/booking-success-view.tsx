66895352d8af1e0d4bd26f8f9ceb6ba4adcae729
dibyamagar56 <dibya.magar@portpro.io>
2024-05-25 23:35:20 +0545

    design: Booking Success page

--- src/sections/success/booking-success-view.tsx
+++ src/sections/success/booking-success-view.tsx
@@ -7,7 +7,7 @@ const BookingSuccess = () => {
     <section className="success-booking-page">
       <div className="container-fluid text-center">
         <Image src={SuccessImg} alt={"Ticket Booking Success Image"} height={400} />
-        <h3 className="confirmation-title mt-5">Booking confirmed successfully!</h3>
+        <h3 className="confirmation-title mt-5">Booking confirmed successfully! 🎉 🎉</h3>
         <p className="message">
           Thank you for choosing to book tickets with Hulya Events! Your reservation is confirmed. If there's anything
           you need before your arrival, please don't hesitate to reach out to your host!
