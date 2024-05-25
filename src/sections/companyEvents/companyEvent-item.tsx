482c3740662c5ceb55b974a358ee6d0a4d0af720
AjayRB5248 <ajayranabhatofficial@gmail.com>
2024-05-25 14:27:34 +0545

    fix:minor bugs

--- src/sections/companyEvents/companyEvent-item.tsx
+++ src/sections/companyEvents/companyEvent-item.tsx
@@ -91,9 +91,6 @@ export default function CompanyEventItem({ event, onView, onEdit, onDelete,onAss
     />
   );
 
-  const venueNames = venues?.map((venue:any) => venue.city).join('-');
-  const venuesDate = venues?.map((venue:any) =>  `${fDate(venue.eventDate)}`).join('-');
-
   const renderInfo = (
     <Stack
       spacing={1.5}
@@ -112,7 +109,7 @@ export default function CompanyEventItem({ event, onView, onEdit, onDelete,onAss
           icon: <Iconify icon="mingcute:location-fill" sx={{ color: 'error.main' }} />,
         },
         {
-          label: venues && venues[0]?.eventDate ? moment(venues[0]?.eventDate).format('MMMM D, h A') : '',
+          label: venues && venues[0]?.eventDate ? moment(venues[0]?.eventDate).format('MMMM D, h A') : 'No Date Added Yet',
           icon: <Iconify icon="solar:clock-circle-bold" sx={{ color: 'info.main' }} />,
         },
         {
