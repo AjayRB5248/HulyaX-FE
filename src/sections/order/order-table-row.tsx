0b579336242b749fef0be55bd0307da29b6c8a70
AjayRB5248 <ajayranabhatofficial@gmail.com>
2024-05-31 16:46:10 +0545

    fix:timezone

--- src/sections/order/order-table-row.tsx
+++ src/sections/order/order-table-row.tsx
@@ -20,6 +20,7 @@ import { useSnackbar } from 'notistack';
 // types
 // components
 import moment from 'moment';
+import 'moment-timezone';
 import { useState } from 'react';
 import { isValidEmail } from 'src/auth/context/jwt/utils';
 import Iconify from 'src/components/iconify';
@@ -52,6 +53,7 @@ const sendTicket = (data: any) => {
 };
 
 export default function OrderTableRow({ row }: Props) {
+  console.log({row})
   const [downloading, setDownloading] = useState(false);
   const [sending, setSending] = useState(false);
   const [sendTicketModal, setSendTicketModal] = useState(false);
@@ -122,7 +124,7 @@ export default function OrderTableRow({ row }: Props) {
 
         <TableCell>
           {row?.venues
-            ?.map((item: any) => moment(item?.eventDate).format('MMMM DD, YYYY h:mm:ss A Z'))
+            ?.map((item: any) => moment(item?.eventDate).tz(row?.stateData?.[0]?.timeZone).format('MMMM DD, YYYY h:mm:ss A '))
             .join(', ')}
         </TableCell>
         <TableCell>
